2baad8e5fda8288509ffd625cbc534a7215c2a62
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-03-17 17:06:44 -0600

    first commit

---
+++ bin/dart_application_login.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_login/dart_application_login.dart' as dart_application_login;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_login.calculate()}!');
+}
---
+++ test/dart_application_login_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_login/dart_application_login.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
