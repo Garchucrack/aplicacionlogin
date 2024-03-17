b9adc5458c73a2055e8cdf9852a54c985b85ee3b
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-03-17 17:25:58 -0600

    importar los paquetes a las carpetas e inicializar el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,59 @@
-int calculate() {
-  return 6 * 7;
+import 'package:dart_application_auntentication1/routes/myroutes.dart';
+import 'package:dart_application_auntentication1/routes/rourte.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'slayer',
+      initialRoute: MyRoutes.home.name,
+      routes: routes,
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => PageNotFound(name: settings.name),
+        );
+      },
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black, 
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.red),
+          bodyText2: TextStyle(color: Colors.red),
+        ),
+      ),
+    );
+  }
+}
+
+class PageNotFound extends StatelessWidget {
+  const PageNotFound({Key? key, required this.name}) : super(key: key);
+  final String? name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'La ruta $name no se encuentra',
+              style: TextStyle(color: Colors.red),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, MyRoutes.home.name);
+              },
+              child: const Text('regresar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/pages.dart/home.dart
@@ -0,0 +1,3 @@
+
+import 'package:flutter/material.dart';
+import 'package:dart_application_auntentication1/routes/myroutes.dart';
